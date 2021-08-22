7a51f0de62ed3d4e9a89cbbbf39dbb5cf3f628fa
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-08-22 23:57:45 +0200

    allConstruct Tabulation added

--- dynamicProgrammingSamples/allConstruct.js
+++ dynamicProgrammingSamples/allConstruct.js
@@ -32,7 +32,25 @@ const allContructMem = (target, wordBank, memo = {}) => {
     memo[target] = result;
     return result;
 }
+
+const allConstructTab = (target, wordBank) => {
+    const tab = Array(target.length + 1).fill([]);
+    tab[0] = [[]];
+    for(let i = 0; i <= target.length; i++) {
+        for(let word of wordBank) {
+            if(target.slice(i, i + word.length) == word) {
+                tab[i + word.length] = tab[i + word.length].concat(tab[i].map( way =>  [word].concat(way)));
+            }
+        }
+    }
+    return tab[target.length];
+}
+
     
 
-console.log(allContruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // [["abc","def"]]
-console.log(allContructMem("purple", ["purp", "p", "ur", "le", "purpl"]));  
+/* console.log(allContruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // [["abc","def"]]
+console.log(allContructMem("purple", ["purp", "p", "ur", "le", "purpl"]));   */
+
+
+console.log(allConstructTab("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // [["abc","def"]]
+console.log(allConstructTab("purple", ["purp", "p", "ur", "le", "purpl"]));  
