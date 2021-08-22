9941f8869be806567267b0c457509a358c6ccea5
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-08-22 20:58:22 +0200

    canSum Tabulation added

--- dynamicProgrammingSamples/canSum.js
+++ dynamicProgrammingSamples/canSum.js
@@ -24,13 +24,29 @@ const canSumMem = (target, collection, memo = {}) => {
     return false;
 }
 
+const canSumTab = (target, collection) => {
+    const tab = Array(target + 1).fill(false);
+    tab[0] = true;
+    for(let num of collection){
+        tab[num] = true;
+        for(let i = num; i <= target; i+=num){
+            tab[i] = true;
+        }
+    }
+    return tab[target];
+}
 
 /* console.log(canSum(5, [1, 2, 3, 4, 5])) // true
 console.log(canSum(7, [2, 4])) // false
 console.log(canSum(8, [2, 3, 5])) // true
 console.log(canSum(300, [7, 14])) // false */
 
-console.log(canSumMem(5, [1, 2, 3, 4, 5])) // true
+/* console.log(canSumMem(5, [1, 2, 3, 4, 5])) // true
 console.log(canSumMem(7, [2, 4])) // false
 console.log(canSumMem(8, [2, 3, 5])) // true
-console.log(canSumMem(300, [7, 14])) // false 
+console.log(canSumMem(300, [7, 14])) // false  */
+
+console.log(canSumTab(5, [1, 2, 3, 4, 5])) // true
+console.log(canSumTab(7, [2, 4])) // false
+console.log(canSumTab(8, [2, 3, 5])) // true
+console.log(canSumTab(300, [7, 14])) // false 
