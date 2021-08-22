607878fb24437f9dc1ca2e1fa1bedc6b26742b45
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-08-22 15:20:18 +0200

    gridTraveller tabulation

--- dynamicProgrammingSamples/gridTraveller.js
+++ dynamicProgrammingSamples/gridTraveller.js
@@ -13,11 +13,26 @@ const gridTravellerMem = (n, m, memo = {})  => {
     return memo[`${n}_${m}`];
 }
 
+const gridTravellerTab = (row, column)  => {
+    let gridTravellerTab = Array(row+1).fill(null).map( el => el = Array(column+1).fill(0));
+    gridTravellerTab[1][1] = 1;
+    for(let i = 0; i <= row; i++) {
+        for(let j = 0; j <= column; j++) {
+            if(i+1 <= row) gridTravellerTab[i+1][j] += gridTravellerTab[i][j];
+            if(j+1 <= column) gridTravellerTab[i][j+1] += gridTravellerTab[i][j];
+        }
+    }
+    return gridTravellerTab[row][column];
+}
+
 
 /* console.log(gridTraveller(3, 3)) // 6
 console.log(gridTraveller(18, 18)) // 137846528820
 console.log(gridTraveller(1, 1)) // 1 */
 
-console.log(gridTravellerMem(3, 3)) // 6
+/* console.log(gridTravellerMem(3, 3)) // 6
 console.log(gridTravellerMem(18, 18)) // 137846528820
-console.log(gridTravellerMem(1, 1)) // 1
+console.log(gridTravellerMem(1, 1)) // 1  */
+
+console.log(gridTravellerTab(3, 3));
+console.log(gridTravellerTab(18, 18)) // 137846528820
